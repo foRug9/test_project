7abaaf945d81b778fbf4c5f02148f63fee8dc362
Vladimir Morgunov <vladimir.morgunov.83@yandex.ru>
2024-03-15 00:01:20 +0300

    a simple update

--- temp.py
+++ temp.py
@@ -1,44 +1,37 @@
 """Модуль для отработки учебного кода"""
-# temp.py
+
+from random import randint
 
 
-class Product:
-    """Продукт"""
-    def __init__(self, name, retail_price, purchase_price):
-        self.name = name
-        self.retail_price = retail_price
-        self.purchase_price = purchase_price
+class ParentClass:
+    """Класс для демонстрации декоратора @classmethod"""
 
-    @property
-    def profit(self):
-        """Свойство продукта: разница между закупочной и розничной ценой"""
-        return self.purchase_price - self.retail_price
+    value: list = []
 
-    @staticmethod
-    def average_price(price_list):
-        """Статистический метод: возвращает средне значение цен из списка"""
-        return ((sum(price for price in price_list)) /
-                len(price_list) if price_list else 0)
+    def __init__(self) -> None:
+        pass
 
-    @property
-    def information(self):
-        """Информация о товаре"""
-        return (f'Товар: {str.capitalize(self.name)}, '
-                f'розничная цена: {self.retail_price}, '
-                f'закупочная цена: {self.purchase_price}')
+    @classmethod
+    def update_value(cls, new_value):
+        """Метода для обновления значения value"""
+        cls.value.append(new_value)
+        if cls.value[0] != cls.value[-1]:
+            cls.value.pop(0)
 
 
-# Данные для проверки, не изменяйте их.
-product_1 = Product('Картошка', 100, 90)
-product_2 = Product('Перчатки', 150, 120)
-product_3 = Product('Велосипед', 170, 150)
+class DaughterClass1(ParentClass):
+    """Дочерний класс 1"""
 
-assortment_prices = [
-    product_1.purchase_price,
-    product_2.purchase_price,
-    product_3.purchase_price
-]
 
-print(f'Средняя стоимость: {Product.average_price(assortment_prices)}')
-print(f'Прибыль магазина с товара {product_1.name}: {product_1.profit}')
-print(f'Информация о товаре {product_1.name}: {product_1.information}')
+class DaughterClass2(ParentClass):
+    """Дочерний класс 2"""
+
+
+object1 = DaughterClass1()
+object2 = DaughterClass2()
+
+print(object1.value, object2.value)
+for _ in range(3):
+    temp = randint(0, 100)
+    object1.update_value((temp, temp + 1))
+    print(object1.value, object2.value)
